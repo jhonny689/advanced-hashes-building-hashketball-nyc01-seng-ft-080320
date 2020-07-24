d9615bb6dfd9e98d69769e5f482c839b1c1f3234
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-24 20:17:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,13 +14,93 @@ def game_hash
         steals: 0,
         blocks: 0,
         slam_dunks: 0
+      },{
+        player_name: "Reggie Evans",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Brook Lopez",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Manson Plumlee",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Jason Terry",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
       }]
     },
     away: {
       team_name: "",
       colors: [],
       players: [{
-        player_name: "",
+        player_name: "Jeff Adrien",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Bismack Biyombo",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "DeSagna Diop",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Ben Gordon",
+        number: 0,
+        shoe: 0,
+        points: 0,
+        rebounds: 0,
+        assists: 0,
+        steals: 0,
+        blocks: 0,
+        slam_dunks: 0
+      },{
+        player_name: "Kemba Walker",
         number: 0,
         shoe: 0,
         points: 0,
